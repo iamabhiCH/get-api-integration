dc72b751e306abeb44427c748688bdf4f422d9ae
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-27 12:12:57 +0530

    Get API integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = React.useState();
+
+    const APICall = async () => {
+        try {
+            const res = await axios.get("https://dummyjson.com/products");
+            console.log(res.data);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    useEffect(() => {
+        APICall();
+    }, []);
+
+    return (
+        <div>
+            <p>This is an API inegration. ( GET Method )</p>
+        </div>
+    );
 }
 
 export default App;
