6e3446cda32d42e512c37b1cc509f6156373b35e
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-30 11:27:15 +0530

    Set the data as output on web page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-    const [data, setData] = React.useState();
+    const [data, setData] = useState([]);
 
     const APICall = async () => {
         try {
             const res = await axios.get("https://dummyjson.com/products");
-            console.log(res.data);
+            console.log(res.data.products);
+            setData(res.data.products);
         } catch (e) {
             console.log(e);
         }
@@ -19,9 +20,25 @@ function App() {
     }, []);
 
     return (
-        <div>
-            <p>This is an API inegration. ( GET Method )</p>
-        </div>
+        <>
+            <div>
+                <h1>This is an API inegration. ( GET Method )</h1>
+            </div>
+            <div className="container">
+                <div>
+                    <ul>
+                        {data.map((dt) => (
+                            <li key={dt.id}>
+                                <h2>Name : {dt.title}</h2>
+                                <p>Description : {dt.description}</p>
+                                <p>Price : {dt.price}</p>
+                                <p>Discount : {dt.discountPercentage}%</p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </>
     );
 }
 
